907b5451a115a8804062e9ad7dd3de1f3c8bd0e2
Hein Htet <heinhtet25944@gmail.com>
2019-12-17 10:05:23 +0630

    Thue

--- src/features/pages/components/employeeTable.js
+++ src/features/pages/components/employeeTable.js
@@ -1,19 +1,85 @@
 import React from 'react'
 import Sidebar from '../../app/sidebar.js'
+import MyButton from '../../../tools/myButton.js'
 
-const EmployeeTable =()=>{
+const EmployeeTable =props=>{
+    const { EmployeeData, ImgUrl,fatherName,dateOfBirth,nrcNo,joinDate,departmentId,designationId,education,gender,martialStatus,Address,createdDate,employeeName}=props;
     return(
-        
-    <div className='text-center'>
-        <div>
-                <Sidebar />
-            </div>
-        <div>
-            Hello
+<div>
+    <Sidebar />
+    <div className='container py -5'>
+        <MyButton
+            className="my-2"
+            text={"+ Add New Employee"}
+            // onClick={_handleAddNew}
+            type={"submit"}
+            style={{ borderRadius: 5, fontSize: 18 }}
+      />
+      <div className='table-responsive'>
+        <table className="table table-dark text-light text-left">
+                <thead>
+                <tr>
+                    <th style={{maxwidth:'70px'}}>Employee Image</th>
+                    <th>Employee Name</th>
+                    <th>Father Name</th>
+                    <th>DateOfBirth</th>
+                    <th>NRC No</th>
+                    <th>Join Date</th>
+                    <th>DepartmentId</th>
+                    <th>DesignationId</th>
+                    <th>Education</th>
+                    <th>Gender</th>
+                    <th>MartialStatus</th>
+                    <th>Address</th>
+                    <th>CREATE BY</th>
+                    <th>CreatedDate</th>
+                </tr>
+                </thead>
+                <tbody>
+              {EmployeeData.map((v, k) => (
+                <tr key={k}>
+                    <td>{k + 1}</td>
+                    <td> <img className='img-fluid'
+                            src={process.env.PUBLIC_URL + `${v.ImgUrl}`}
+                            id={v.id}
+                            alt="styles"
+                          /></td>
+                    <td>{v.employeeName}</td>
+                    <td>{v.fatherName}</td>
+                    <td>{v.dateOfBirth}</td>
+                    <td>{v.nrcNo}</td>
+                    <td>{v.joinDate}</td>
+                    <td>{v.departmentId}</td>
+                    <td>{v.designationId}</td>
+                    <td>{v.education}</td>
+                    <td>{v.gender}</td>
+                    <td>{v.martialStatus}</td>
+                    <td>{v.address}</td>
+                    <td>{v.createdDate}</td>
+                    <td>{v.employeeName}</td>
+                    <td>
+                    <button
+                      type={"button"}
+                    //   onClick={() => onOpenModal(v, k)}
+                      style={{
+                        borderRadius: "8px",
+                        backgroundColor: "#c7821c",
+                        color: "white",
+                        width: "80px"
+                      }}
+                    >
+                      Edit
+                    </button>
+                  </td>
+                </tr>
+              ))}
+                </tbody>
+        </table>
+      
+      </div>
+     
         </div>
-  
         </div>
-   
     )
 }
 export default EmployeeTable
--- src/features/pages/container/employeeContainer.js
+++ src/features/pages/container/employeeContainer.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import Sidebar from '../../app/sidebar.js'
+import EmployeeTable from '../components/employeeTable.js'
+import Navbar from '../../app/navbar'
 
 const EmployeeContainer =()=>{
     return(
         
-    <div className='text-center'>
+    <div className='text-center py-5'>
+        <Sidebar />
+        <Navbar/>
         <div>
-                <Sidebar />
-            </div>
-        <div>
-            Hello
+            <EmployeeTable EmployeeData={Employee}/>
         </div>
-  
-        </div>
-   
+    </div>   
     )
 }
 export default EmployeeContainer
+
+const Employee= [
+    {
+        id:1,employeeImage:'',employeeName:'Aung Aung',fatherName:'U Mya', dateOfBirth:'7-9-1998', nrcNo:'9/mahama(N)049871', joinDate:'1-10-2019', departmentId:'1', designationId:'1', education:'Graduat', gender:'Male', martialStatus:'Single', address:'35 Street', createdBY:'1', createdDate:'12-11-2019'
+    }
+]
